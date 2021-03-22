40eac4dacad71871321b8af9a4eb10198fb537cf
FabianN98 <80277063+FabianN98@users.noreply.github.com>
2021-03-22 09:46:41 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -63,6 +63,6 @@ function LED1_Off(){
   // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
-	  document.getElementById("sensor").innerHTML="payloadString";
+	  document.getElementById("sensor").innerHTML="message.payloadString";
   }
   
